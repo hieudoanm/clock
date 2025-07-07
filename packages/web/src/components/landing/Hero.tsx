61e299b8aee661b6285c18886b30fb127c0a1752
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-07-07 15:37:20 +0700

    add tauri

--- packages/web/src/components/landing/Hero.tsx
+++ packages/web/src/components/landing/Hero.tsx
@@ -1,3 +1,4 @@
+import { LinearText } from '@clock/components/shared/Linear';
 import { APP_NAME } from '@clock/constants';
 import Link from 'next/link';
 import { FC } from 'react';
@@ -6,13 +7,17 @@ export const Hero: FC = () => {
 	return (
 		<section className="w-full py-20">
 			<div className="mx-auto flex max-w-3xl flex-col gap-y-4 text-center md:gap-y-8">
-				<h2 className="text-4xl font-extrabold text-neutral-100 sm:text-5xl">{APP_NAME}</h2>
-				<p className="text-lg text-neutral-500">
+				<h2 className="text-4xl font-extrabold text-neutral-100 sm:text-5xl">
+					<LinearText>{APP_NAME}</LinearText>
+				</h2>
+				<p className="text-lg text-neutral-100">
 					A sleek, timezone-aware clock and Pomodoro timer — stay focused and in sync, right from your browser.
 				</p>
 				<div>
 					<Link href="/pomodoro">
-						<button type="button" className="cursor-pointer rounded-full border border-neutral-800 px-6 py-3">
+						<button
+							type="button"
+							className="cursor-pointer rounded-full border border-neutral-700 bg-neutral-900 px-6 py-3">
 							Launch App
 						</button>
 					</Link>
--- packages/web/src/pages/index.tsx
+++ packages/web/src/pages/index.tsx
@@ -2,21 +2,25 @@ import { CallToAction } from '@clock/components/landing/CallToAction';
 import { Features } from '@clock/components/landing/Features';
 import { Footer } from '@clock/components/landing/Footer';
 import { Hero } from '@clock/components/landing/Hero';
+import { LinearBackground } from '@clock/components/shared/Linear';
 import { Navbar } from '@clock/components/shared/Navbar';
 import { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
 	return (
 		<>
-			<Navbar />
-			<div className="w-full border-t border-neutral-800" />
-			<Hero />
-			<div className="w-full border-t border-neutral-800" />
-			<Features />
-			<div className="w-full border-t border-neutral-800" />
-			<CallToAction />
-			<div className="w-full border-t border-neutral-800" />
-			<Footer />
+			<LinearBackground />
+			<div className="relative z-10">
+				<Navbar />
+				<div className="w-full border-t border-neutral-800" />
+				<Hero />
+				<div className="w-full border-t border-neutral-800" />
+				<Features />
+				<div className="w-full border-t border-neutral-800" />
+				<CallToAction />
+				<div className="w-full border-t border-neutral-800" />
+				<Footer />
+			</div>
 		</>
 	);
 };
