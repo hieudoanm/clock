1d09c41b1fe751cc46c7899727eb0a733cae873b
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-07-11 19:19:30 +0700

    update button

--- packages/web/src/components/landing/CallToAction.tsx
+++ packages/web/src/components/landing/CallToAction.tsx
@@ -14,7 +14,7 @@ export const CallToAction: FC = () => {
 					<Link href="/pomodoro">
 						<button
 							type="button"
-							className="cursor-pointer rounded-full border border-neutral-700 bg-neutral-900 px-6 py-3">
+							className="cursor-pointer rounded-full border border-neutral-700 bg-neutral-800 px-6 py-3">
 							Launch App
 						</button>
 					</Link>
